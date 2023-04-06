a73b48aacb492d091e89d0f981e6833e6e4b59df
PavelVovk <106928926+Maytata@users.noreply.github.com>
2023-04-06 18:21:48 +0300

    Update SEM5_HW.java

--- SEM5_HW.java
+++ SEM5_HW.java
@@ -1,3 +1,5 @@
+
+//Первая задача 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -46,5 +48,127 @@ public class TASK01 {
         for (Map.Entry<String, ArrayList<Integer>> entry : contacts_list.entrySet()) {
             System.out.println(entry.getKey() + ": тел. " + entry.getValue());
         }
+    }
+}
+
+
+//Вторая задача
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class TASK02 {
+    public static void main(String[] args) {
+        String[] employee_list = {"Иван Иванов","Светлана Петрова","Кристина Белова","Анна Мусина",
+                "Анна Крутова","Иван Юрин","Петр Лыков","Павел Чернов","Петр Чернышов","Мария Федорова",
+                "Марина Светлова","Мария Савина","Мария Рыкова","Марина Лугова","Анна Владимирова","Иван Мечников",
+                "Петр Петин","Иван Ежов"};
+
+        Map<String, Integer> employee_counter = new HashMap<>();
+
+        for (String employee : employee_list){
+            int counter;
+            String[] name = employee.split(" ");
+            employee_counter.putIfAbsent(name[0], 1);
+            if (employee_counter.containsKey(name[0])) {
+                counter = employee_counter.get(name[0]);
+                counter ++;
+                employee_counter.put(name[0], counter);
+
+            }
+        }
+
+        Map<String, Integer> names_sorted_list = new LinkedHashMap<>();
+        while (!employee_counter.isEmpty()) {
+            int common_name_counter = 0;
+            String name_found = "";
+            for (Map.Entry<String, Integer> entry : employee_counter.entrySet()) {
+                if (common_name_counter < entry.getValue()){
+                    common_name_counter = entry.getValue();
+                    name_found = entry.getKey();
+                }
+            }
+
+            names_sorted_list.put(name_found, common_name_counter);
+            employee_counter.remove(name_found);
+        }
+
+        for (Map.Entry<String, Integer> item : names_sorted_list.entrySet()) {
+            System.out.println(item.getKey() + ": " + item.getValue());
+        }
+    }
+}
+
+
+//Третья задача 
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TASK03 {
+    public static void main(String[] args) {
+        int queens = 0;
+        char[][] clear = new char[8][8];
+        char[][] desk = new char[8][8];
+        Random rnd = new Random();
+        int place_the_queen;
+
+        while (queens < 8){
+            ArrayList<Integer> queen_arr = new ArrayList<>();
+
+            for (int c = 0; c < 8; c++){
+                if (desk[queens][c] != 'n'){
+                    queen_arr.add(c);
+                }
+            }
+
+            if (queen_arr.size() == 0){
+                queens = 0;
+                for (int i = 0; i < desk.length; i ++){
+                    System.arraycopy(clear[i], 0, desk[i], 0, desk.length);
+                }
+                for (int c = 0; c < 8; c++){
+                    if (desk[queens][c] != 'n'){
+                        queen_arr.add(c);
+                    }
+                }
+            }
+
+            place_the_queen = rnd.nextInt(0, queen_arr.size());
+
+            desk[queens][queen_arr.get(place_the_queen)] = 'x';
+
+            int shift = 1;
+            for (int k = queens + 1; k < desk.length; k ++){
+                for (int m = 0 ; m < desk.length; m ++){
+                    if (m == queen_arr.get(place_the_queen) && k != queens) {
+                        desk[k][m] = 'n';
+                    }
+                    if (m != queen_arr.get(place_the_queen) && k == queens) {
+                        desk[k][m] = 'n';
+                    }
+                    if ((m == queen_arr.get(place_the_queen) + shift && k == queens + shift) ||
+                            (m == queen_arr.get(place_the_queen) - shift && k == queens + shift)) {
+                        desk[k][m] = 'n';
+                    }
+                }
+                shift ++;
+            }
+
+            queens ++;
+
+        }
+
+        for (char[] chars : desk) {
+            for (int j = 0; j < desk.length; j++) {
+                if (chars[j] != 'x') {
+                    System.out.print("|" + ' ');
+                } else System.out.print("|" + chars[j]);
+                if (j == desk.length - 1){
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
